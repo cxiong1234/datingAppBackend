16de2ba533a2be021bae6b83016f31c1f76b734a
jhu <jingweihu.usc@gmail.com>
2024-04-07 10:00:56 -0700

    Test

--- src/main/java/org/example/datingapp/Friendship/FriendshipRESTController.java
+++ src/main/java/org/example/datingapp/Friendship/FriendshipRESTController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 public class FriendshipRESTController {
 
+    // Try to modifier the same file to cause merge conflict
     @RequestMapping("/friendship")
     public String checkFriendshipStatus(@RequestParam(name = "userId") Long userId,
                                         @RequestParam(name = "friendId") Long friendId) {
