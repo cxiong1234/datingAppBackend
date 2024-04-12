d92333e6090d5a1cb4d92e55824436e119b8cdda
jhu <jingweihu.usc@gmail.com>
2024-04-12 14:16:39 -0700

    Test

--- src/main/java/org/example/datingapp/Profile/ProfileRESTController.java
+++ src/main/java/org/example/datingapp/Profile/ProfileRESTController.java
@@ -3,6 +3,8 @@ package org.example.datingapp.Profile;
 import org.example.datingapp.Friendship.entities.UserEntity;
 import org.example.datingapp.Friendship.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +16,12 @@ class ProfileRESTController {
     private UserRepository repository;
 
     @RequestMapping("/profile")
-    public ProfileResponse getProfile(
+    public ResponseEntity<ProfileResponse> getProfile(
             @RequestParam(name = "user_id") Integer userId
     ) {
-        return convertToResponse(repository.getReferenceById(userId));
+        return new ResponseEntity<>(
+                convertToResponse(repository.getReferenceById(userId)),
+                HttpStatus.OK);
     }
 
     private ProfileResponse convertToResponse(UserEntity user) {
